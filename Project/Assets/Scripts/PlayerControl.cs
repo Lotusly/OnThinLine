d002b06b6f62d9e540f9a74bf0690b0e3057177f
Lotusly <yul4@andrew.cmu.edu>
2018-04-21 14:05:55 -0400

    base, control (position), history

--- Project/Assets/Scripts/PlayerControl.cs
+++ Project/Assets/Scripts/PlayerControl.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour {
+
+
+
+public class PlayerControl : MonoBehaviour
+{
+
+	private float original;
+	[SerializeField] private LineRenderer history;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		//original = Input.GetAxis("Vertical");
+		original=0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		transform.localPosition = Vector3.up * (Input.GetAxis("Vertical")-original);
+		history.positionCount++;
+		history.SetPosition(history.positionCount-1,transform.position);
+	
 	}
 }
